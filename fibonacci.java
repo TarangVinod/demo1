b19012649da04605401e54f36671750d22ed5c1c
TarangVinod <91175955+TarangVinod@users.noreply.github.com>
2021-09-28 12:14:29 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,22 @@
+package math;
+
+public class fibonacci {
+	public static void main(String[] args) {
+		
+		int a=1,b=0,c,i;
+		
+		for(i=1;i<10;i++) {
+			c=a+b;
+			System.out.println(+c);
+			
+			a=b;
+			b=c;
+			
+			
+					
+		}
+		
+		
+	}
+
+}
